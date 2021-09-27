b40f6fdd222fffeebf90eb6e1290eeb04b6b8ba5
David Fernandez <isaacdavid959@gmail.com>
2021-09-26 18:14:49 -0600

    TAREA CLASE PACIENTE

---
+++ POOClass/Form1.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace POOClass
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            label1.Text = "Escriba el nombre del paciente";
+            button1.Text = "Agregar";
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Patient paciente = new Patient();
+            paciente.setName(txtName.Text);
+            paciente.setSick(txtSick.Text);
+            paciente.setYears(txtYears.Text);
+            paciente.setNumber(txtNumber.Text);
+
+            //agregar datos a ListBox
+            listBox1.Items.Add(paciente.getDates());
+
+            //limpiar los textBox
+            txtName.Text="";
+            txtSick.Text="";
+            txtYears.Text="";
+            txtNumber.Text="";
+            txtName.Focus();
+
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            //Limpiar el TextBox
+            listBox1.Items.Clear();
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+
+            this.Close();
+        }
+    }
+}
---
+++ POOClass/Patient.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace POOClass
+{
+    class Patient
+    {
+        //definir miembros de la clase
+        //propiedades,atributos datos
+        private string _name;
+        private string _NumberPhone;
+        private string _Years;
+        private string _sickNess;
+
+        
+        public Patient()
+        { }
+        public Patient(string name, string numberPhone, string years, string sick) //Metodo contructor
+        {
+            _name = name;
+            _NumberPhone = numberPhone;
+            _Years = years;
+            _sickNess = sick;
+
+        }
+
+        //Nombre
+        public void setName(string name)
+        {
+            _name = name;
+        }
+
+        public string getName()
+        {
+            return _name;
+        }
+
+        //NumberPhone
+        public void setNumber(string numberPhone)
+        {
+            _NumberPhone = numberPhone;
+        }
+
+        public string getNumber()
+        {
+            return _NumberPhone;
+        }
+
+        // Años
+        public void setYears(string years)
+        {
+            _Years = years;
+        }
+
+        public string getYears()
+        {
+            return _Years;
+        }
+
+        //Enfermedad
+        public void setSick(string sick)
+        {
+            _sickNess = sick;
+        }
+
+        public string getSick()
+        {
+            return _sickNess;
+        }
+
+        //Metodo
+        public string getDates()
+        {
+            return "Nombre: " + _name +"Años: "+_Years+"Numero de telefono"+_NumberPhone+"Enfermedad"+_sickNess;
+        }
+        
+        
+
+    }
+}
